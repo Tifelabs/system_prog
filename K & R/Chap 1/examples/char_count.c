0d87b9bb04bf045aac29cb926e6e1cc35a1e1d12
Tifelabs <boluwatifeolusegun018@gmail.com>
2025-01-06 15:15:35 -0400

    Folder name change

---
+++ K & R/Chap 1/examples/char_count.c
@@ -0,0 +1,36 @@
+//Character counting
+//A program that counts character
+
+#include <stdio.h>
+
+/*Counting characters in input first version*/
+
+int main(){
+
+    long nc;
+    
+    nc =  0;
+
+    while (getchar() != EOF){
+        ++nc;
+        printf("%1d\n",nc);
+    }
+}
+
+
+/* NOTE => Long integers are at atleast 32 bits  */
+
+//Self Test
+#include <stdio.h>
+
+int main (){
+
+    long nc;
+
+    nc = 0;
+
+    while (getchar() != EOF){
+        nc++;
+        printf("Characters: %1d",nc);
+    }
+}

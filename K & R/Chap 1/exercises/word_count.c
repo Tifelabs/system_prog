ef00d7159bc07dff869f69346aca7e9f23857fbb
Tifelabs <boluwatifeolusegun018@gmail.com>
2025-01-23 23:43:03 -0400

    feat : Study!

---
+++ K & R/Chap 1/exercises/word_count.c
@@ -0,0 +1,20 @@
+/* 
+Write a program that prints its input one word per line
+ */
+
+#include <stdio.h>
+
+int main () {
+
+    int c;
+    int word;
+
+    while((c == getchar() != EOF)) {
+        
+        putchar(word)
+    }
+
+
+    printf("%d\n", word);
+
+}

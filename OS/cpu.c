62a9092a5f3e5b2a919fca9904f7061cc7bea6a0
Tifelabs <boluwatifeolusegun018@gmail.com>
2025-05-15 21:28:12 -0300

    OS:: cpu.c clean up

--- OS/cpu.c
+++ OS/cpu.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <assert.h>
-#include "common.h"
+#include "include/common.h"
 #include <unistd.h>
 
-int main(int argc, char *agrv[]){
+int main(int argc, char *argv[]){
     if(argc != 2){
         fprintf(stderr, "usage: cpu <string>\n");
         exit(1);
     }
     char *str = argv[1];
     while(1){
-        spin(1);
+        Spin(1);
         printf("%s\n", str);
     }
     return 0;
